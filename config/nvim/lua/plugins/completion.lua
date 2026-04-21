7d12248443576b5350986e4d329d0127c0f64e65
rito528 <39003544+rito528@users.noreply.github.com>
2026-04-21 23:22:07 +0900

    feat: Update blink-cmp keymap to super-tab

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -7,6 +7,9 @@ return {
       "rafamadriz/friendly-snippets",
     },
     opts = {
+      keymap = {
+        preset = "super-tab",
+      },
       appearance = {
         nerd_font_variant = "mono",
       },
