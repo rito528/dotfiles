3b90d206215cec8bc3edb20d43fdb826cad4129c
rito528 <39003544+rito528@users.noreply.github.com>
2026-04-23 00:22:03 +0900

    refactor: rust 専用の highlights 設定を削除

--- modules/neovim/base.nix
+++ modules/neovim/base.nix
@@ -2,9 +2,6 @@
   lib,
   ...
 }:
-let
-  rustHighlightsQuery = builtins.readFile ../../config/nvim/after/queries/rust/highlights.scm;
-in
 {
   globals = {
     mapleader = " ";
@@ -232,29 +229,25 @@ in
   };
 
   extraConfigLuaPre = ''
-        function _G.statuscolumn_numbers()
-          if vim.v.virtnum ~= 0 then
-            return ""
-          end
+    function _G.statuscolumn_numbers()
+      if vim.v.virtnum ~= 0 then
+        return ""
+      end
 
-          local absolute = tostring(vim.v.lnum)
-          local relative = tostring(vim.v.relnum)
+      local absolute = tostring(vim.v.lnum)
+      local relative = tostring(vim.v.relnum)
 
-          return string.format("%4s %4s ", absolute, relative)
-        end
+      return string.format("%4s %4s ", absolute, relative)
+    end
 
-        for _, runtime in ipairs({
-          vim.env.NVIM_TREESITTER_RUNTIME_GLOBAL,
-          vim.env.NVIM_TREESITTER_RUNTIME_PROJECT,
-        }) do
-          if runtime and runtime ~= "" then
-            vim.opt.rtp:append(runtime)
-          end
-        end
-
-        vim.treesitter.query.set("rust", "highlights", [=[
-    ${rustHighlightsQuery}
-    ]=])
+    for _, runtime in ipairs({
+      vim.env.NVIM_TREESITTER_RUNTIME_GLOBAL,
+      vim.env.NVIM_TREESITTER_RUNTIME_PROJECT,
+    }) do
+      if runtime and runtime ~= "" then
+        vim.opt.rtp:append(runtime)
+      end
+    end
   '';
 
   autoCmd = [
